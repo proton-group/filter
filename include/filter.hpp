ab699db15b8966048b119d47ca9f575073e3bb1e
Anatoly Alexandrov <33666129+proton-group@users.noreply.github.com>
2022-08-16 08:01:47 +0700

    Add files via upload

---
+++ include/filter.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "BitmapPlusPlus.hpp"
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+int mfilter(std::stringstream& in, std::stringstream& out);
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "filter.hpp"
+
+const int MINPARAM = 2;
+
+int main(int argc, char* argv[])
+{
+    if(argc < MINPARAM)
+    {
+        return -1;
+    }
+    std::string buf;
+    std::string data[4];
+    std::stringstream in_str;
+    std::stringstream out_str;
+    std::ifstream in(argv[1]); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        int i = 0;
+        while(std::getline(in, buf))
+        {
+            in_str << buf + "\n";
+            i++;
+        }
+    }
+    in.close();
+    return mfilter(in_str, out_str);
+}
